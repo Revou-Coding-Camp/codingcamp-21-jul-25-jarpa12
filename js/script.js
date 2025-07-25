273fb1945e06d5b040433d6f5c67b2af8e8d82f5
jarpa12 <m.fajardd21@gmail.com>
2025-07-26 02:28:33 +0700

    mini projek 2
    projek done

---
+++ js/script.js
@@ -0,0 +1,70 @@
+console.log("Hello, World!");
+let tasks = [];
+let showCompletedOnly = false; // Variable to track filter state
+function addTask() {
+    //Function to add a task
+    const taskInput = document.getElementById("task");
+    const dateInput = document.getElementById("Date");
+    // Check if inputs are empty
+    if (taskInput.value === "" || dateInput.value === "") {
+        alert("Please fill in both task and date fields.");
+    } else {
+    tasks.push({
+        task: taskInput.value,
+        date: dateInput.value,
+        completed: false,
+    });
+
+    console.log("Task added:", taskInput.value, "on", dateInput.value);
+    console.log(tasks);
+    }
+
+    renderTasks();
+}
+
+function removeAllTask() {
+    //Function to remove a task
+    tasks = [];
+
+    renderTasks();
+}
+
+function removeTask(index) {
+    //Function to remove a specific task
+    tasks.splice(index, 1);
+    console.log("Task removed:", index);
+    renderTasks();
+}
+
+function completeTask(index) {
+    //Function to mark a task as complete
+    tasks[index].completed = true;
+    console.log("Task completed:", tasks[index].task);
+    renderTasks();
+}
+
+function toggleFilter() {
+    //Function to toggle the filter
+   showCompletedOnly = !showCompletedOnly;
+   renderTasks();
+}
+
+function renderTasks() {
+    //Function to render tasks in the task list
+    const taskList = document.getElementById("task-list");
+    taskList.innerHTML = ""; // Clear the current list
+
+    let filteredTasks = showCompletedOnly ? tasks.filter(task => task.completed) : tasks;
+    filteredTasks
+    filteredTasks.forEach((task, index) => {
+        taskList.innerHTML += 
+        `<li class="task-item flex justify-between items-center py-2">
+            <span>${task.task} ${task.date}</span>
+            <div>
+                    <button type="button" class="px-[12px] py-[4px] rounded-[6px] bg-green-500 text-white" onclick="completeTask(${index});">Complete</button>
+                    <button class="px-[12px] py-[4px] rounded-[6px] bg-red-500 text-white" onclick="removeTask(${index});">Delete</button>
+            </div>
+        </li>
+        `;
+    });
+}
